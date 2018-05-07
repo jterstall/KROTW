1d580d880a0b4f4baa1101fdf788eada82c04b3f
jterstall <jeroen_t@hotmail.com>
2018-05-07 15:36:46 +0200

    Commitment issues
    Co-Authored-By: tychokoster <tychokoster@users.noreply.github.com>

--- milestone2/main.py
+++ milestone2/main.py
@@ -8,9 +8,11 @@ import jellyfish
 import time
 import re
 
+LIMIT = 10
+
 def dist_ranking(fn, concept, cls, inverted=True):
     scores = [(fn(concept, str(concept2).split('.')[-1]), concept2) for concept2 in cls]
-    return sorted(scores, key=lambda x : x[0], reverse=inverted)
+    return sorted(scores, key=lambda x: x[0], reverse=inverted)
 
 
 def dist_ranking_syns(fn, syn_A, cls, syns_B, inverted=True):
@@ -18,14 +20,12 @@ def dist_ranking_syns(fn, syn_A, cls, syns_B, inverted=True):
         scores = [(max(retrieve_scores(fn, syn_A, syn_B)), cls[i]) for i, syn_B in enumerate(syns_B)]
     else:
         scores = [(min(retrieve_scores(fn, syn_A, syn_B)), cls[i]) for i, syn_B in enumerate(syns_B)]
-    return sorted(scores, key=lambda x : x[0], reverse=inverted)
+    return sorted(scores, key=lambda x: x[0], reverse=inverted)
 
 
-def ranking_similarity(ont_A, ont_B, syns_A, syns_B):
+def ranking_similarity(classes_A, classes_B, syns_A, syns_B):
     pprint("Ranking concept similarity")
     timein = time.time()
-    classes_A = list(ont_A.classes())
-    classes_B = list(ont_B.classes())
     ranking_hamming = []
     ranking_jaro = []
     ranking_waterman = []
@@ -33,21 +33,23 @@ def ranking_similarity(ont_A, ont_B, syns_A, syns_B):
     ranking_jaro_syns = []
     ranking_waterman_syns = []
     for i, concept in enumerate(classes_A):
-        ranking_hamming.append((concept, dist_ranking(jellyfish.hamming_distance, str(concept).split('.')[-1], classes_B, inverted=False)))
-        ranking_jaro.append((concept, dist_ranking(jellyfish.jaro_distance, str(concept).split('.')[-1], classes_B)))
-        ranking_waterman.append((concept, dist_ranking(smith_waterman, str(concept).split('.')[-1], classes_B)))
-        syns_concept = syns_A[i]
-        ranking_hamming_syns.append((concept, dist_ranking_syns(jellyfish.hamming_distance, syns_concept, classes_B, syns_B, inverted=False)))
-        ranking_jaro_syns.append((concept, dist_ranking_syns(jellyfish.jaro_distance, syns_concept, classes_B, syns_B)))
-        ranking_waterman_syns.append((concept, dist_ranking_syns(smith_waterman, syns_concept, classes_B, syns_B)))
+        if i < LIMIT:
+            ranking_hamming.append((concept, dist_ranking(jellyfish.hamming_distance, str(concept).split('.')[-1], classes_B, inverted=False)))
+            ranking_jaro.append((concept, dist_ranking(jellyfish.jaro_distance, str(concept).split('.')[-1], classes_B)))
+            ranking_waterman.append((concept, dist_ranking(smith_waterman, str(concept).split('.')[-1], classes_B)))
+            syns_concept = syns_A[i]
+            ranking_hamming_syns.append((concept, dist_ranking_syns(jellyfish.hamming_distance, syns_concept, classes_B, syns_B, inverted=False)))
+            ranking_jaro_syns.append((concept, dist_ranking_syns(jellyfish.jaro_distance, syns_concept, classes_B, syns_B)))
+            ranking_waterman_syns.append((concept, dist_ranking_syns(smith_waterman, syns_concept, classes_B, syns_B)))
     for i, concept in enumerate(classes_B):
-        ranking_hamming.append((concept, dist_ranking(jellyfish.hamming_distance, str(concept).split('.')[-1], classes_A, inverted=False)))
-        ranking_jaro.append((concept, dist_ranking(jellyfish.jaro_distance, str(concept).split('.')[-1], classes_A)))
-        ranking_waterman.append((concept, dist_ranking(smith_waterman, str(concept).split('.')[-1], classes_A)))
-        syns_concept = syns_B[i]
-        ranking_hamming_syns.append((concept, dist_ranking_syns(jellyfish.hamming_distance, syns_concept, classes_A, syns_A, inverted=False)))
-        ranking_jaro_syns.append((concept, dist_ranking_syns(jellyfish.jaro_distance, syns_concept, classes_A, syns_A)))
-        ranking_waterman_syns.append((concept, dist_ranking_syns(smith_waterman, syns_concept, classes_A, syns_A)))
+        if i < LIMIT:
+            ranking_hamming.append((concept, dist_ranking(jellyfish.hamming_distance, str(concept).split('.')[-1], classes_A, inverted=False)))
+            ranking_jaro.append((concept, dist_ranking(jellyfish.jaro_distance, str(concept).split('.')[-1], classes_A)))
+            ranking_waterman.append((concept, dist_ranking(smith_waterman, str(concept).split('.')[-1], classes_A)))
+            syns_concept = syns_B[i]
+            ranking_hamming_syns.append((concept, dist_ranking_syns(jellyfish.hamming_distance, syns_concept, classes_A, syns_A, inverted=False)))
+            ranking_jaro_syns.append((concept, dist_ranking_syns(jellyfish.jaro_distance, syns_concept, classes_A, syns_A)))
+            ranking_waterman_syns.append((concept, dist_ranking_syns(smith_waterman, syns_concept, classes_A, syns_A)))
     timeout = time.time()
     pprint("Run time: " + str(timeout - timein) + " seconds")
     return ranking_hamming, ranking_jaro, ranking_waterman, ranking_hamming_syns, ranking_jaro_syns, ranking_waterman_syns
@@ -64,7 +66,7 @@ def create_triples(cls_A, cls_B, indices):
 def compute_exact_match_syns(match_indices, concept1, concept2, i, j):
     common = list(set(concept1).intersection(set(concept2)))
     if common:
-        match_indices.append((i,j))
+        match_indices.append((i, j))
     return match_indices
 
 
@@ -96,11 +98,12 @@ def compute_synonym_similarity(syns_A, syns_B, hamming_threshold=1, jaro_thresho
     waterman_indices = []
     match_indices = []
     for i, concept1 in enumerate(syns_A):
-        for j, concept2 in enumerate(syns_B):
-            match_indices = compute_exact_match_syns(match_indices, concept1, concept2, i, j)
-            hamming_indices = compute_dist_syns_le(jellyfish.hamming_distance, hamming_indices, concept1, concept2, i, j, hamming_threshold)
-            jaro_indices = compute_dist_syns_ge(jellyfish.jaro_distance, jaro_indices, concept1, concept2, i, j, jaro_threshold)
-            waterman_indices = compute_dist_syns_ge(smith_waterman, waterman_indices, concept1, concept2, i, j, waterman_threshold)
+        if i < LIMIT:
+            for j, concept2 in enumerate(syns_B):
+                match_indices = compute_exact_match_syns(match_indices, concept1, concept2, i, j)
+                hamming_indices = compute_dist_syns_le(jellyfish.hamming_distance, hamming_indices, concept1, concept2, i, j, hamming_threshold)
+                jaro_indices = compute_dist_syns_ge(jellyfish.jaro_distance, jaro_indices, concept1, concept2, i, j, jaro_threshold)
+                waterman_indices = compute_dist_syns_ge(smith_waterman, waterman_indices, concept1, concept2, i, j, waterman_threshold)
     timeout = time.time()
     pprint("Run time: " + str(timeout - timein) + " seconds")
     return match_indices, hamming_indices, jaro_indices, waterman_indices
@@ -116,9 +119,7 @@ def fetch_synonym(cls, syns):
     return syns
 
 
-def convert_to_synonyms(ont_A, ont_B):
-    classes_A = list(ont_A.classes())
-    classes_B = list(ont_B.classes())
+def convert_to_synonyms(classes_A, classes_B):
     syns_A = []
     syns_B = []
     for cls in classes_A:
@@ -147,11 +148,11 @@ def compute_dist_le(fn, matches, match_indices, concept1, concept2, i, j, thresh
     dist = fn(concept1, concept2)
     if dist <= threshold:
         matches.append((concept1, concept2))
-        match_indices.append((i,j))
+        match_indices.append((i, j))
     return matches, match_indices
 
 
-def compute_string_similarity(ont_A, ont_B, hamming_threshold=1, jaro_threshold=0.95, waterman_threshold=5):
+def compute_string_similarity(classes_A, classes_B, hamming_threshold=1, jaro_threshold=0.95, waterman_threshold=5):
     pprint("Calculating concept similarity")
     timein = time.time()
     jaro_matches = []
@@ -162,16 +163,15 @@ def compute_string_similarity(ont_A, ont_B, hamming_threshold=1, jaro_threshold=
     waterman_indices = []
     matches = []
     match_indices = []
-    classes_A = list(ont_A.classes())
-    classes_B = list(ont_B.classes())
     for i, concept1 in enumerate(classes_A):
-        concept1 = str(concept1).split('.')[-1]
-        for j, concept2 in enumerate(classes_B):
-            concept2 = str(concept2).split('.')[-1]
-            matches, match_indices = compute_exact_match(concept1, concept2, matches, match_indices, i, j)
-            hamming_matches, hamming_indices = compute_dist_le(jellyfish.hamming_distance, hamming_matches, hamming_indices, concept1, concept2, i, j, hamming_threshold)
-            jaro_matches, jaro_indices = compute_dist_ge(jellyfish.jaro_distance, jaro_matches, jaro_indices, concept1, concept2, i, j, jaro_threshold)
-            waterman_matches, waterman_indices = compute_dist_ge(smith_waterman, waterman_matches, waterman_indices, concept1, concept2, i, j, waterman_threshold)
+        if i < LIMIT:
+            concept1 = str(concept1).split('.')[-1]
+            for j, concept2 in enumerate(classes_B):
+                concept2 = str(concept2).split('.')[-1]
+                matches, match_indices = compute_exact_match(concept1, concept2, matches, match_indices, i, j)
+                hamming_matches, hamming_indices = compute_dist_le(jellyfish.hamming_distance, hamming_matches, hamming_indices, concept1, concept2, i, j, hamming_threshold)
+                jaro_matches, jaro_indices = compute_dist_ge(jellyfish.jaro_distance, jaro_matches, jaro_indices, concept1, concept2, i, j, jaro_threshold)
+                waterman_matches, waterman_indices = compute_dist_ge(smith_waterman, waterman_matches, waterman_indices, concept1, concept2, i, j, waterman_threshold)
     timeout = time.time()
     pprint("Run time: " + str(timeout - timein) + " seconds")
     return match_indices, hamming_indices, jaro_indices, waterman_indices
@@ -185,28 +185,31 @@ def load_ontologies(path1, path2):
 
 
 def main():
-    path1 = "file://animalsA.owl"
-    path2 = "file://animalsB.owl"
+    path1 = "file://people+petsA.owl"
+    path2 = "file://people+petsB.owl"
     ont_A, ont_B = load_ontologies(path1, path2)
+    classes_A = list(ont_A.classes())
+    classes_B = list(ont_B.classes())
 
-    match_indices, hamming_indices, jaro_indices, waterman_indices = compute_string_similarity(ont_A, ont_B)
+    match_indices, hamming_indices, jaro_indices, waterman_indices = compute_string_similarity(classes_A, classes_B)
 
-    syns_A, syns_B = convert_to_synonyms(ont_A, ont_B)
+    syns_A, syns_B = convert_to_synonyms(classes_A, classes_B)
     syn_match_indices, syn_hamming_indices, syn_jaro_indices, syn_waterman_indices = compute_synonym_similarity(syns_A, syns_B)
 
-    match_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), match_indices)
-    syn_match_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), syn_match_indices)
+    match_triples = create_triples(classes_A, classes_B, match_indices)
+    syn_match_triples = create_triples(classes_A, classes_B, syn_match_indices)
 
-    hamming_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), hamming_indices)
-    syn_hamming_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), syn_hamming_indices)
+    hamming_triples = create_triples(classes_A, classes_B, hamming_indices)
+    syn_hamming_triples = create_triples(classes_A, classes_B, syn_hamming_indices)
 
-    jaro_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), jaro_indices)
-    syn_jaro_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), syn_jaro_indices)
+    jaro_triples = create_triples(classes_A, classes_B, jaro_indices)
+    syn_jaro_triples = create_triples(classes_A, classes_B, syn_jaro_indices)
 
-    waterman_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), waterman_indices)
-    syn_waterman_triples = create_triples(list(ont_A.classes()), list(ont_B.classes()), syn_waterman_indices)
+    waterman_triples = create_triples(classes_A, classes_B, waterman_indices)
+    syn_waterman_triples = create_triples(classes_A, classes_B, syn_waterman_indices)
 
-    hamming_ranking, jaro_ranking, waterman_ranking, hamming_syns_ranking, jaro_syns_ranking, waterman_syns_ranking = ranking_similarity(ont_A, ont_B, syns_A, syns_B)
+    hamming_ranking, jaro_ranking, waterman_ranking, hamming_syns_ranking, jaro_syns_ranking, waterman_syns_ranking = ranking_similarity(classes_A, classes_B, syns_A, syns_B)
+
 
 if __name__ == '__main__':
     main()
